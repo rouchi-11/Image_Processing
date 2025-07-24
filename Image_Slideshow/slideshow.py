92fcadd162b57eff3f15a78ffdc425bc415e903c
rouchi-11 <141231824+rouchi-11@users.noreply.github.com>
2025-07-25 01:25:51 +0530

    Add files via upload

---
+++ Image_Slideshow/slideshow.py
@@ -0,0 +1,40 @@
+# ./slideshow.py
+
+import cv2
+import numpy as np
+from math import ceil
+import os
+
+dst = "./images/"       # Images destination
+images = os.listdir(dst)    # Get their names in a list
+length = len(images)
+
+result = np.zeros((360,360,3), np.uint8)      # Image window of size (360, 360)
+i = 1
+
+a = 1.0     # alpha
+b = 0.0     # beta
+img = cv2.imread(dst + images[i])
+img = cv2.resize(img, (360, 360))
+
+# Slide Show Loop
+while(True):
+
+    if(ceil(a)==0):
+        a = 1.0
+        b = 0.0
+        i = (i+1)%length    # Getting new image from directory
+        img = cv2.imread(dst + images[i])
+        img = cv2.resize(img, (360, 360))
+
+    a -= 0.01
+    b += 0.01
+
+    # Image Transition from one to another
+    result = cv2.addWeighted(result, a, img, b, 0)
+    cv2.imshow("Slide Show", result)
+    key = cv2.waitKey(1) & 0xff
+    if key==ord('q'):
+        break
+
+cv2.destroyAllWindows()
